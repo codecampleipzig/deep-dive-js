c020ed6e1c102ecd36ba32c6c9f50a1e95b4cc78
Gabriel Heinrich <gabriel@codecampleipzig.de>
2019-12-04 00:50:10 +0100

    Solutions for Array challenges

--- 2-data-structures/challenges.js
+++ 2-data-structures/challenges.js
@@ -2,19 +2,23 @@
 function spliceChallenge() {
    const daysInWeek = ["Tuesday", "Fizz", "Buzz", "Wednesday", "_", "Friday"]  // don't touch this!
    // Add calls to splice here:
-
+   daysInWeek.splice(1, 2);
+   daysInWeek.splice(2, 1, "Thursday");
+   daysInWeek.splice(0, 0, "Monday");
+   daysInWeek.splice(5, 0, "Saturday", "Sunday");
    return daysInWeek;
 }
 
 // This code doesn't work, but with a tiny tweak it will.
 function highScore() {
    const scores = [62, 234, 163, 32, 41, 272];
-   return Math.max(scores);
+   return Math.max(...scores);
 }
 
 // From the glossary (alphabetically sorted list of strings), extract a subsection for a specific starting letter
 // e.g. entriesStartingWith(["Adagio", "Andante", "Breve", "Cadenza", "Chorale", "Dolce"], "C") -> ["Cadenza", "Chorale"]
 function entriesStartingWith(glossary, startingLetter) { 
+   return glossary.filter(s => s.startsWith(startingLetter));
 }
 
 // Without touching the array literals, and without adding any new string literals, return this array:
@@ -23,63 +27,118 @@ function sliceSpreadChallenge() {
    const x = ["X", "A", "B", "C", "X"];
    const y = ["E", "F", "G", "X"];
    const z = ["X", "C", "D", "E"];
+   return [...x.slice(1, 3), ...z.slice(1, 3), ...y.slice(0, 3)]
 }
 
 // Sort an array of values, by type in this order: First booleans, than numbers, than strings.
 // Apart from that, the order is undefined
 // sortChallenge(['a', true, 3]) -> [true, 3, 'a']
 function sortChallenge(values) {
-
+   values.sort((lhs, rhs) => {
+      if (typeof lhs == "boolean") {
+         if (typeof rhs == "boolean") {
+            return 0
+         }
+         else {
+            return -1;
+         }
+      }
+      if (typeof lhs == "number") {
+         if (typeof rhs == "boolean") {
+            return 1;
+         }
+         if (typeof rhs == "number") {
+            return 0;
+         }
+         else {
+            return -1;
+         }
+      }
+      if (typeof lhs == "string") {
+         if (typeof rhs == "string") {
+            return 0;
+         }
+         else {
+            return 1;
+         }
+      }
+   })
+   return values;
 }
 
 // Implement a function equivalent to Array.prototype.find with a loop
 // If no element satisfies the condition return undefined
 function find(array, predicate) {
-
+   for (element of array) {
+      if (predicate (element)) {
+         return element;
+      }
+   }
+   return undefined;
 }
 
 // Implement lastIndexOf with a loop
 function lastIndexOf(array, element) {
-
+   for (let i = array.length - 1; i >= 0; i--) {
+      if (array[i] === element)
+         return i;
+   }
+   return -1;
 }
 
 // Implement includes using indexOf
 function includes(array, element) {
-
+   return array.indexOf(element) != -1;
 }
 
 // Write a function that takes in an array of strings and checks whether each of the strings contain the letter 'A'
 function everyContainsA(strings) {
-
+   return strings.every(s => s.includes('A'))
 }
 // Write a function that takes in an array of strings and checks whether one of the strings contains a number
 function someContainsNumber(strings) {
-
+   function containsNumber(string) {
+      for (let i = 0; i < string.length; ++i) {
+         if ("0123456789".includes(string.charAt(i))) {
+            return true;
+         }
+      }
+      return false;
+   }
+   // REGEX version
+   return strings.some(containsNumber);
 }
 
 // Implement Array.prototype.some with a loop
 function some(array, condition) {
-
+   for (element of array) {
+      if (condition(element)) {
+         return true;
+      }
+   }
+   return false;
 }
 
 // Create a function that adds a string ending to each member in an array.
 // e.g. addEnding(["clever", "meek", "hurried", "nice"], "ly")
 // -> ["cleverly", "meekly", "hurriedly", "nicely"]
 function addEnding(array, ending) {
-
+   return array.map (s => s + ending);
 }
 
 // Create a function to remove all null values from an array.
 // e.g. removeNull(["a", null, "b", null]) -> ["a", "b"]
 function removeNull(array) {
-
+   return array.filter(e => e !== null);
 }
 
 // Using reduce in combination with the concat method, write a function to flatten an array of arrays into a single array, that has all the elements of the original orrays
 // e.g. flatten([['a', 'b'], ['c', 'd'], ['e']])
 // -> ['a', 'b', 'c', 'd', 'e']
 function flatten(array) {
-
+   return array.reduce((result, element) => {
+      return [...result, ...element];
+   }, [])
 }
 
 module.exports = {
--- 2-data-structures/challenges.test.js
+++ 2-data-structures/challenges.test.js
@@ -3,6 +3,7 @@ const {
    highScore, 
    sliceSpreadChallenge, 
    entriesStartingWith,
+   sortChallenge,
    find,
    lastIndexOf,
    includes,
@@ -15,17 +16,17 @@ const {
 } = require("./challenges")
 
 describe ("2 Data Structures", () => {
-   xtest("spliceChallenge", () => {
+   test("spliceChallenge", () => {
       expect(spliceChallenge()).toEqual(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
    });
-   xtest("highScore", () => {
+   test("highScore", () => {
       expect(highScore()).toBe(272);
    });
-   xtest("sliceSpreadChallenge", () => {
+   test("sliceSpreadChallenge", () => {
       expect(sliceSpreadChallenge()).toEqual(["A", "B", "C", "D", "E", "F", "G"]);
    });
 
-   xtest("entriesStartingWith",() => {
+   test("entriesStartingWith",() => {
       const testGlossary = ["A1", "A2", "A3", "B1", "B2", "D1", "D2"]
       expect(entriesStartingWith(testGlossary, "A")).toEqual(["A1", "A2", "A3"]);
       expect(entriesStartingWith(testGlossary, "B")).toEqual(["B1", "B2"]);
@@ -33,34 +34,34 @@ describe ("2 Data Structures", () => {
       expect(entriesStartingWith(testGlossary, "D")).toEqual(["D1", "D2"]);
    })
 
-   xtest('sortChallenge',() => {
+   test('sortChallenge',() => {
       expect(sortChallenge(['a', true, 0])).toEqual([true, 0, 'a']);
       expect(sortChallenge([true, 0, 'a', 0, 'a', true])).toEqual([true, true, 0, 0, 'a', 'a']);
    })
 
-   xtest('find', () => {
+   test('find', () => {
       expect(find([1, 2, 3], x => x == 2)).toBe(2);
       expect(find([1, 2, 3], x => null)).toBe(undefined);
       expect(find([1, 2, 3], x => x > 2)).toBe(3);
    })
-   xtest('lastIndexOf', () => {
+   test('lastIndexOf', () => {
       expect(lastIndexOf([3, 3, 3], 3)).toBe(2);
       expect(lastIndexOf([3, 3, 3, 1, 1], 3)).toBe(2);
       expect(lastIndexOf([3, 3, 3, 1, 1], 2)).toBe(-1);
    })
-   xtest('includes', () => {
+   test('includes', () => {
       expect(includes([1, 2, 3, 4, 5], 4)).toBeTruthy();
       expect(includes([1, 2, 3, 4, 5], 6)).toBeFalsy();
-      expect(includes([1, 2, 3, 4, 5], "3")).toBeTruthy();
+      expect(includes([1, 2, 3, 4, 5], 3)).toBeTruthy();
    })
-   xtest('everyContainsA', () => {
+   test('everyContainsA', () => {
       expect(everyContainsA([])).toBeTruthy();
       expect(everyContainsA(["BA"])).toBeTruthy();
       expect(everyContainsA(["A", "BA", "CDA"])).toBeTruthy();
       expect(everyContainsA(["A", "BA", "CDA", ""])).toBeFalsy();
    })
 
-   xtest('someContainsNumber', () => {
+   test('someContainsNumber', () => {
       expect(someContainsNumber([])).toBeFalsy();
       expect(someContainsNumber(["AAA", "AAA", "A0A"])).toBeTruthy();
       expect(someContainsNumber(["AAA", "A1AA", "AA"])).toBeTruthy();
@@ -70,26 +71,26 @@ describe ("2 Data Structures", () => {
       expect(someContainsNumber(["AAA", "AAA", "AA"])).toBeFalsy();
    })
 
-   xtest('some', () => {
+   test('some', () => {
       expect(some(['A', 'B', 'C'], v => v === 'C')).toBeTruthy();
       expect(some(['A', 'B', 'C'], v => false)).toBeFalsy();
    })
 
-   xtest('addEnding', () => {
+   test('addEnding', () => {
       expect(addEnding(["clever", "meek", "hurried", "nice"], "ly"))
       .toEqual(["cleverly", "meekly", "hurriedly", "nicely"]);
       expect(addEnding(["new", "pander", "scoop"], "er"))
       .toEqual(["newer", "panderer", "scooper"])
    });
 
-   xtest('removeNull', () => {
+   test('removeNull', () => {
       expect(removeNull([])).toEqual([]);
       expect(removeNull(['A', 'B', 'C'])).toEqual(['A', 'B', 'C']);
       expect(removeNull([null, 'A', null, 'B', null, 'C', null])).toEqual(['A', 'B', 'C']);
       expect(removeNull([null, null, false, "", 0])).toEqual([false, "", 0]);
    })
 
-   xtest('flatten', () => {
+   test('flatten', () => {
       expect(flatten([])).toEqual([]);
       expect(flatten([[1, 2], [3, 4], [5]])).toEqual([1, 2, 3, 4, 5]);
       expect(flatten([[1, 2], [], [3, 4], [5]])).toEqual([1, 2, 3, 4, 5]);
