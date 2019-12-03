696a5c86842bf094b474296cdf488b051fe98a49
programmiana <m.camargomoreira@gmail.com>
2019-12-03 22:37:38 +0100

    do one more excercise

--- 1-foundations/challenges.js
+++ 1-foundations/challenges.js
@@ -23,8 +23,8 @@ function escapeChallenge() {
 // var text = "Hello\\nWorld\\n !"
 
 function addLineNumbers(text) {
-  // text.split('\\n').map((line, index) => `${index + 1}. ${line}`).join('\n');
-  // console.log(text)
+  return text.split("\n").map((line, index) => `${index + 1} ${line}`).join("\n");
+  
 }
 
 // A bugfree version of typeof
@@ -50,41 +50,28 @@ function randomInt(min, max) {
 // If any of the arguments is a string convert them first to a number using parseFloat.
 // If the conversion fails, throw an exception;
 function add(lhs, rhs) {
-   lhs = parseFloat(lhs);
-   rhs = parseFloat(rhs);
-   if ( lhs == NaN || rhs == NaN) {
-      throw new Error();
-   } else {
-      return lhs + rhs;
-   }
+  lhs = parseFloat(lhs);
+  rhs = parseFloat(rhs);
+  if (lhs == NaN || rhs == NaN) {
+    throw new Error();
+  } else {
+    return lhs + rhs;
+  }
 
-
-  if (toString.call(lhs, rhs) !== "[ ]") {return false;
-  }else {
-  var total = 0;
-  for (var i = 0; i < input.length; i++) {
-    if (isNaN(input[i])) {
-      continue;
+  // Print all the numbers from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. For numbers that are divisible by both 5 and 3 print "FizzBuzz".
+  function fizzBuzz() {
+    for (let i = 1; i <= 100; i = i + 1) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz");
+      } else if (i % 3 == 0) {
+        console.log("buzz");
+      } else if (i % 5 == 0) {
+        console.log("buzz");
+      } else {
+        console.log(i);
+      }
     }
-    total += Number(input[i]);
   }
-  return total;
-}
-
-// Print all the numbers from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. For numbers that are divisible by both 5 and 3 print "FizzBuzz".
-function fizzBuzz() {
-for (let i = 1; i <= 100; i = i + 1) {
-   if (i % 3 == 0 && i % 5 == 0) {
-console.log('fizzbuzz'); 
-   } else if (i % 3 == 0) {
-console.log('buzz');
-   } else if (i % 5 == 0) {
-console.log('buzz');
-   } else {
-console.log(i); 
-  }
-}
-}
 }
 // Return a greeting string for the specified language
 // greeting("Anne", "English") -> "Hello Anne!"
@@ -92,15 +79,15 @@ console.log(i);
 // greeting("Carolin", "German") -> "Hallo Carolin!"
 // Throw an exception if the language is not known
 function greeting(name, language) {
-   if (name == "Anne" && language == "English") {
-      return "Hello Anne!";
-   } else if (name == "Bob" && language == "French") {
-      return "Salut Bob!";
-   } else if (name == "Carolin" && language == "German") {
-      return "Hallo Carolin!";
-} else {
-   return "we don't know you and don't speak your language, leave. jk. but we really don't know you, sorry"
-}
+  if (name == "Anne" && language == "English") {
+    return "Hello Anne!";
+  } else if (name == "Bob" && language == "French") {
+    return "Salut Bob!";
+  } else if (name == "Carolin" && language == "German") {
+    return "Hallo Carolin!";
+  } else {
+    return "we don't know you and don't speak your language, leave. jk. but we really don't know you, sorry";
+  }
 }
 
 // Return a random string of lowercase ASCII characters.
@@ -119,11 +106,10 @@ function isVarBinding(binding) {}
 // fileExtension ("index.js") -> "js"
 // fileExtension ("index") => ""
 function fileExtension(filename) {
-   let index = filename.lastIndexOf(".");
-   if (index == "") {
-      return "";
-   } else {
-      return filename.substring(index+1);
-   }
-
+  let index = filename.lastIndexOf(".");
+  if (index == "") {
+    return "";
+  } else {
+    return filename.substring(index + 1);
+  }
 }
