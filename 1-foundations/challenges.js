55d0f61202ed86b4cf562b62209966690b7b4db8
Gabriel Heinrich <gabriel@codecampleipzig.de>
2019-12-04 12:10:59 +0100

    TODO's for 1-foundations challenges

--- 1-foundations/challenges.js
+++ 1-foundations/challenges.js
@@ -13,13 +13,13 @@ module.exports = {
 }
 
 // Return this string \"'``'"\
-function escapeChallenge() {
+function escapeChallenge() { // check
    return "\\\"\'\`\`\'\"\\";
 }
 
 // Add line numbers to the beginning of each line:
 // e.g. addLineNumbers("Hello\nWorld\n!") -> "1 Hello\n2 World\n3 !"
-function addLineNumbers(text) {
+function addLineNumbers(text) { // TODO: put space around operator
    return text.split("\n").map((line, i) => `${i+1} ${line}`).join("\n");
 }
 
@@ -27,7 +27,7 @@ function addLineNumbers(text) {
 // Notes:
 //    typeof null -> "object", but jstypeof(null) -> "null"
 //    typeof () => {} -> "function", but jstypeof(() => {}) -> "object"
-function jstypeof(value) {
+function jstypeof(value) { // check
    if ( value === null ) {
       return "null"
    }
@@ -41,7 +41,7 @@ function jstypeof(value) {
 
 // Return a random number in the range min (inclusive) to max (exclusive)
 // 
-function randomNumber(min, max) {
+function randomNumber(min, max) { // check
    if ( min >= max) {
       throw new Error("max has to be greater min")
    }
@@ -51,7 +51,7 @@ function randomNumber(min, max) {
 
 // Return a random integer between min (inclusive) and max (exclusive)
 // You may assume that min and max are both integers.
-function randomInt(min, max) {
+function randomInt(min, max) { // check
    min = (min < 0) ? min + 1 : min;
    return Math.floor(randomNumber(min, max));
 }
@@ -59,7 +59,7 @@ function randomInt(min, max) {
 // Write a function that takes two arguments and returns their sum.
 // If any of the arguments is a string convert them first to a number using parseFloat.
 // If the conversion fails, throw an exception;
-function add(lhs, rhs) {   
+function add(lhs, rhs) {  // check 
    lhs = parseFloat(lhs);
    rhs = parseFloat(rhs);
 
@@ -71,7 +71,7 @@ function add(lhs, rhs) {
 }
 
 // Print all the numbers from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. For numbers that are divisible by both 5 and 3 print "FizzBuzz".
-function fizzBuzz() {
+function fizzBuzz() { // TODO: put space around operators
    for (let i = 1; i <= 100; i++) {
       if (i%5 == 0 && i%3 == 0) {
          console.log("FizzBuzz");
@@ -97,7 +97,7 @@ function greeting(name, language) {
 
 // Return a random string of lowercase ASCII characters.
 // Notes: Take a look at the string method fromCodePoint()
-function randomLowerCaseString(length) {
+function randomLowerCaseString(length) { // Check
    let output = "";
    for (let i = 0; i < length; i++) {
       let random = randomInt(97, 123);
@@ -109,7 +109,7 @@ function randomLowerCaseString(length) {
 // Given a Javascript binding, return whether or not it is a var binding.
 // isVarBinding("var x = 10;") -> true
 // isVarBinding("let x = 10;") -> false
-function isVarBinding(binding) {
+function isVarBinding(binding) { // TODO: write an else block to throw an exception, or just return binding.startsWith("var")
    if (binding.startsWith("var")) {
       return true
    }
@@ -122,7 +122,7 @@ function isVarBinding(binding) {
 // fileExtension ("Readme.md") -> "md"
 // fileExtension ("index.js") -> "js"
 // fileExtension ("index") => ""
-function fileExtension(filename) {
+function fileExtension(filename) { // TODO: Space around operators
    let parts = filename.split(".");
    if ( parts[1] ) {
       return parts[parts.length-1];
